c2c056a525d3246effd816e7e46c864615a68af1
minnya <minnya9018@yahoo.co.jp>
2025-10-27 08:37:54 +0900

    Web版をビルド

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1383188300"
+    serviceWorkerVersion: "1793079655"
   }
 });
